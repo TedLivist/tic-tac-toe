c985fde3570ea8ba202109f589930bf5b6c07172
unknown <livistteddy@gmail.com>
2021-05-07 14:22:25 +0100

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,37 +1,37 @@
 #!/usr/bin/env ruby
+
+# rubocop: disable Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity, Style/IdenticalConditionalBranches
 def board(arr)
-  puts "+---+---+---+"
+  puts '+---+---+---+'
   puts "| #{arr[0]} | #{arr[1]} | #{arr[2]} |"
-  puts "+---+---+---+"
+  puts '+---+---+---+'
   puts "| #{arr[3]} | #{arr[4]} | #{arr[5]} |"
-  puts "+---+---+---+"
+  puts '+---+---+---+'
   puts "| #{arr[6]} | #{arr[7]} | #{arr[8]} |"
-  puts "+---+---+---+"
+  puts '+---+---+---+'
 end
 
 def player_assignment
-  puts "Please fill in your name, Player 1"
-  player_1 = gets.chomp
-  puts "Please fill in your name, Player 2"
-  player_2 = gets.chomp
-  [player_1, player_2]
+  puts 'Please fill in your name, Player 1'
+  player_one = gets.chomp
+  puts 'Please fill in your name, Player 2'
+  player_two = gets.chomp
+  [player_one, player_two]
 end
 
 def play
-  cell = [1,2,3,4,5,6,7,8,9]
+  cell = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   board(cell)
   players = player_assignment
   (0..8).each do |i|
     if i.even?
       puts "It's #{players[0]}'s turn"
       begin
-        puts "Please put in a number between 1 and 9"
+        puts 'Please put in a number between 1 and 9'
         num = gets.chomp.to_i
-        if num.nil? or num < 1 or num > 9 or cell[num - 1] != num
-          raise StandardError, num
-        end
+        raise StandardError, num if num.nil? or num < 1 or num > 9 or cell[num - 1] != num
       rescue StandardError
-        puts "invalid input"
+        puts 'invalid input'
         retry
       end
       cell[num - 1] = 'X'
@@ -39,13 +39,11 @@ def play
     else
       puts "It's #{players[1]}'s turn"
       begin
-        puts "Please put in a number between 1 and 9"
+        puts 'Please put in a number between 1 and 9'
         num = gets.chomp.to_i
-        if num.nil? or num < 1 or num > 9 or cell[num - 1] != num
-          raise StandardError, num
-        end
+        raise StandardError, num if num.nil? or num < 1 or num > 9 or cell[num - 1] != num
       rescue StandardError
-        puts "invalid input"
+        puts 'invalid input'
         retry
       end
       cell[num - 1] = 'O'
@@ -55,3 +53,5 @@ def play
 end
 
 play
+
+# rubocop: enable Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity, Style/IdenticalConditionalBranches
