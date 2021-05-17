14e017516ffc05bcc986790b45d3cdcc33bf62cf
unknown <livistteddy@gmail.com>
2021-05-17 09:24:57 +0100

    Write test for board not to include '10'

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,11 @@
-require_relative './lib/board'
+require_relative '../lib/board.rb'
 
 describe Board do
   init_board = Board.new
   describe '#board' do
     it "returns initial first element in the cell" do
       new_board = init_board.board([1,2,3,4,5,6,7,8,9])
-      expect(new_board).to include('9')
+      expect(new_board).not_to include('10')
     end
   end
 end
