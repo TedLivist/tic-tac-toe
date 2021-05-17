63eb32501e1c37b35c4ec8bdd08543a3b3877b8b
unknown <livistteddy@gmail.com>
2021-05-17 09:26:53 +0100

    Refactor first example title for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,10 @@ require_relative '../lib/board.rb'
 describe Board do
   init_board = Board.new
   describe '#board' do
-    it "returns initial first element in the cell" do
+    it "does not include number not passed in" do
       new_board = init_board.board([1,2,3,4,5,6,7,8,9])
       expect(new_board).not_to include('10')
     end
+
   end
 end
