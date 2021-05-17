10acae6bc1bbcd4c0a141001f27322ce63f65389
unknown <livistteddy@gmail.com>
2021-05-17 09:38:03 +0100

    Add test for checking inclusion of item passed in

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,5 +8,10 @@ describe Board do
       expect(new_board).not_to include('10')
     end
 
+    it "includes item passed in" do
+      new_board = init_board.board(['X', 2, 3, 4, 5, 6, 7, 8, 9])
+      expect(new_board).to include('X')
+    end
+
   end
 end
